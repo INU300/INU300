1bc5d15c9f648900cbf44c79540c5965ce49ac24
hen715 <hen715@naver.com>
2023-09-27 17:55:01 +0900

    사용자 역할 추가

---
+++ community/src/main/java/com/sw300/community/config/Role.java
@@ -0,0 +1,16 @@
+package com.sw300.community.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("ROLE_GUEST","손님"),
+    USER("ROLE_USER","일반사용자"),
+    ADMIN("ROLE_ADMIN","관리자");
+
+    private final String key;
+    private final String title;
+}
