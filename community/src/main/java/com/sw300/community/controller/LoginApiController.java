461c8056e3382fa26765f054efc2bc5ea9342f3b
hen715 <hen715@naver.com>
2023-09-27 15:44:24 +0900

    로그인 api 컨트롤러

---
+++ community/src/main/java/com/sw300/community/controller/LoginApiController.java
@@ -0,0 +1,19 @@
+package com.sw300.community.controller;
+
+import com.sw300.community.dto.MemberSaveDto;
+import com.sw300.community.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LoginApiController {
+
+    private final MemberService memberService;
+    @PostMapping("/api/join")
+    public Long newMem(@RequestBody MemberSaveDto memberSaveDto){
+        return memberService.joinMember(memberSaveDto);
+    }
+}
