3d25badf12236b700716b66141ea02a1e5efc2d6
hen715 <hen715@naver.com>
2023-09-27 17:54:33 +0900

    회원정보 jpa 추가

--- community/src/main/java/com/sw300/community/repository/MemberRepository.java
+++ community/src/main/java/com/sw300/community/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.sw300.community.repository;
 import com.sw300.community.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByEmail(String email);
 }
