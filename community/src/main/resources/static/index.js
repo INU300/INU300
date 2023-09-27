6621a79051d28485dbc13cbaa8eb95e63de2dde1
hen715 <hen715@naver.com>
2023-09-27 15:43:43 +0900

    데이터 json 변환 파일

---
+++ community/src/main/resources/static/index.js
@@ -0,0 +1,32 @@
+var main={
+    init : function(){
+        var _this = this;
+        $('#btn-join').on('click',function (){
+            _this.join();
+        });
+    },
+    join : function (){
+        var data = {
+            email: $('#email').val(),
+            password: $('#password').val(),
+            name: $('#name').val(),
+            school: $('#school').val(),
+            department: $('#department').val()
+
+        };
+        $.ajax({
+            type: 'POST',
+            url: '/api/join',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('회원이 등록되었습니다.');
+            window.location.href = '/mainLogin';
+        }).fail(function (error){
+            alert(JSON.stringify(error));
+        });
+    }
+
+};
+main.init();
