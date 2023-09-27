70b266ddd8921e7a70522f24450dfff2f078dc44
hen715 <hen715@naver.com>
2023-09-27 15:44:38 +0900

    로그인 컨트롤러

--- community/src/main/java/com/sw300/community/controller/LoginController.java
+++ community/src/main/java/com/sw300/community/controller/LoginController.java
@@ -1,10 +1,18 @@
 package com.sw300.community.controller;
 
+import com.sw300.community.dto.MemberSaveDto;
+import com.sw300.community.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
+
+
 
     @GetMapping("/mainLogin")
     public String mainLogin(){
@@ -15,4 +23,6 @@ public class LoginController {
     public String join(){
         return "joinPage";
     }
+
+
 }
