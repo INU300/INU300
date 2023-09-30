6f4ee324d0f9ce1890390db96594af0c84e6763b
hann0079 <haeun8701@gmail.com>
2023-09-30 23:27:08 +0900

    feat: 카테고리 자동 분류 기능 추가
    html category에 분류 결과 제공
    gpt-3.5-turbo 모델 사용
    api key 파일 분리 미반영

--- community/src/main/java/com/sw300/community/controller/BoardApiController.java
+++ community/src/main/java/com/sw300/community/controller/BoardApiController.java
@@ -4,12 +4,30 @@ import com.sw300.community.dto.ResponseDto;
 import com.sw300.community.model.Board;
 import com.sw300.community.service.BoardService;
 import io.swagger.annotations.ApiOperation;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.slf4j.Logger;  // 추가
+import org.slf4j.LoggerFactory;  // 추가
+
+import java.util.Map;
 
 @RestController
 public class BoardApiController {
+
+    private static final String OPENAI_URL = "https://api.openai.com/v1/chat/completions";
+    private static final String OPENAI_API_KEY = "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";
+    private static final Logger logger = LoggerFactory.getLogger(BoardApiController.class);  // 로거 인스턴스 생성
+
 
     @Autowired
     private BoardService boardService;
@@ -19,6 +37,49 @@ public class BoardApiController {
     public ResponseDto<Integer> save(@RequestBody Board board) {
         boardService.writePost(board);
         return new ResponseDto<>(HttpStatus.OK, 1);
+    }
+
+    @PostMapping("/api/externalClassify")
+    public ResponseEntity<String> classifyContent(@RequestBody Map<String, String> requestData) {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(OPENAI_URL);
+
+            JSONObject json = new JSONObject();
+            json.put("model", "gpt-3.5-turbo");
+
+            JSONArray messages = new JSONArray();
+
+            // 원래의 내용만 API에 전달
+            JSONObject contentMessage = new JSONObject();
+            contentMessage.put("role", "user");
+            contentMessage.put("content", "제목은 " + requestData.get("title") +
+                    "이고, 내용은 " + requestData.get("content") +
+                    "이야. 이 글을 연애게시판/학업게시판/진로게시판 중에서 카테고리 분류해줘. 대답은 '무슨게시판' 이렇게 단어만 말해줘. 다른 말은 하지 말고.");
+            messages.put(contentMessage);
+
+            System.out.println("Content Message: " + contentMessage.toString());
+
+            json.put("messages", messages);
+
+            StringEntity entity = new StringEntity(json.toString(), "UTF-8"); // UTF-8 인코딩 지정
+            httpPost.setEntity(entity);
+            httpPost.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+            httpPost.setHeader("Authorization", "Bearer " + OPENAI_API_KEY);
+
+            String responseString = EntityUtils.toString(httpClient.execute(httpPost).getEntity(), "UTF-8");
+            logger.info("OpenAI Response: " + responseString);
+
+            JSONObject responseJson = new JSONObject(responseString);
+
+            JSONArray choices = responseJson.getJSONArray("choices");
+            String content = choices.getJSONObject(0).getJSONObject("message").getString("content").trim();
+
+            String firstWord = content.split(" ")[0];
+            return ResponseEntity.ok(firstWord); // 받아온 응답 그대로 반환
+        } catch (Exception e) {
+            logger.error("Error while classifying content:", e);  // 오류 로깅
+            return ResponseEntity.status(500).body("Error: " + e.getMessage());
+        }
     }
 
     @ApiOperation("글 삭제")
@@ -34,4 +95,6 @@ public class BoardApiController {
         boardService.updatePost(id,board);
         return new ResponseDto<>(HttpStatus.OK, 1);
     }
+
+
 }
--- community/src/main/resources/static/js/saveForm.js
+++ community/src/main/resources/static/js/saveForm.js
@@ -1,5 +1,46 @@
-$(document).ready(function() {
-    $('#boardForm').on('submit', function(event) {
+$(document).ready(function () {
+    console.log("Document is ready");
+    console.log($("#classifyButton").length);  // 1이 출력되어야 합니다.
+
+    // Function to classify the content using external API
+    function classifyContent(title, content, callback) {
+        $.ajax({
+            type: 'POST',
+            url: '/api/externalClassify',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify({ title: title, content: content }),
+            success: function (response) {
+                if (response && response.length > 0) {
+                    callback(null, response);
+                } else {
+                    console.error("Server responded without a valid category:", response);
+                    callback(new Error('Classification failed.'));
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error("Error when calling /api/externalClassify:", textStatus, errorThrown);
+                callback(new Error('Classification failed.'));
+            }
+        });
+    }
+
+    $(document).on('click', '#classifyButton', function() {
+
+        console.log("classifyButton clicked");  // 이 부분 추가
+
+        const title = $("input[name='title']").val();
+        const content = $("textarea[name='content']").val();
+
+        classifyContent(title, content, function(err, category) {
+            if (err) {
+                alert('Classification failed.');
+            } else {
+                $("input[name='category']").val(category);
+            }
+        });
+    });
+
+    $('#boardForm').on('submit', function (event) {
         event.preventDefault();
         const formData = {
             title: $("input[name='title']").val(),
@@ -13,10 +54,10 @@ $(document).ready(function() {
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(formData),
             dataType: 'json',
-            success: function(response) {
+            success: function (response) {
                 if (response.status === 200) {
                     alert('게시글이 저장되었습니다.');
-                    window.location.href = '/';  // 게시글 목록 페이지로 리다이렉트
+                    window.location.href = '/';
                 } else {
                     alert('오류가 발생했습니다.');
                 }
