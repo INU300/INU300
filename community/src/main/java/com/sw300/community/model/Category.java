d84c1c76fcb4e81d5644f29823de644ff2c23aa4
ImKEISS <kss5122@naver.com>
2023-10-02 23:32:35 +0900

    게시판 카테고리별 게시글 기능 및 게시글, 로그인, 게시판, 메인 화면 링크 추가

---
+++ community/src/main/java/com/sw300/community/model/Category.java
@@ -0,0 +1,21 @@
+package com.sw300.community.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
