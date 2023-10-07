dda29dd45edb626084f7b5c50ebef86f02cee402
hen715 <hen715@naver.com>
2023-10-08 05:26:20 +0900

    비밀번호 찾기 추가

--- community/src/main/java/com/sw300/community/controller/MemberController.java
+++ community/src/main/java/com/sw300/community/controller/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Map;
 
 
 @Controller
@@ -41,6 +42,25 @@ public class MemberController {
         return "myPage";
     }
 
+    @GetMapping("/searchId")
+    public String searchId(){
+        return "searchIdForm";
+    }
+
+    @GetMapping("/searchPassword")
+    public String searchPassword(){
+        return "searchPasswordForm";
+    }
+
+    @PostMapping("/searchPassword")
+    public String changePassword(@RequestParam("email")String email,@RequestParam("name") String name, Model model){
+        if(memberService.searchPassword(email,name)){
+            model.addAttribute("email",email);
+            return "newPasswordForm";
+        }
+        else
+            return "searchPasswordForm";
+    }
 
 
 
