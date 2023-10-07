6e7691dba62fddea18d84437c125f4bc0624c62d
hen715 <hen715@naver.com>
2023-10-08 02:32:39 +0900

    비밀번호, 닉네임 변경 추가

--- community/src/main/java/com/sw300/community/service/MemberService.java
+++ community/src/main/java/com/sw300/community/service/MemberService.java
@@ -2,12 +2,14 @@ package com.sw300.community.service;
 
 
 import com.sw300.community.dto.MemberSaveDto;
+import com.sw300.community.model.Member;
 import com.sw300.community.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +24,36 @@ public class MemberService {
         /* 소분류 저장 로직*/
         memberSaveDto.setPassword(passwordEncoder.encode(memberSaveDto.getPassword()));
         return memberRepository.save(memberSaveDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Member getMember(String email){
+        Optional<Member> member = memberRepository.findByEmail(email);
+        if(member.isPresent()){
+            return member.get();
+        }
+        else{
+            throw new IllegalStateException("사용자가 없습니다.");
+        }
+    }
+
+    @Transactional
+    public Long changeNickname(Long id,String newNickname){
+         Member mem = memberRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 회원이 존재하지 않습니다."));
+         mem.changeNickname(newNickname);
+        return mem.getId();
+    }
+
+    @Transactional
+    public Long changePassword(Long id,String currentPassword,String newPassword){
+        Member mem = memberRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 회원이 존재하지 않습니다."));
+        if(!passwordEncoder.matches(currentPassword,mem.getPassword())){
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+        else{
+            mem.changePassword(passwordEncoder.encode(newPassword));
+        }
+        return mem.getId();
     }
 
 }
