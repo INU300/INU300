e2803325003f9a9a0bc4ad021bacab2a45deecd3
ImKEISS <kss5122@naver.com>
2023-10-11 21:35:44 +0900

    LoginController -> MemberController home 메서드 코드 복사

--- community/src/main/java/com/sw300/community/controller/MemberController.java
+++ community/src/main/java/com/sw300/community/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.sw300.community.controller;
 
+import com.sw300.community.dto.CategoryDto;
 import com.sw300.community.dto.MemberInformationDto;
 import com.sw300.community.model.Member;
+import com.sw300.community.service.CategoryService;
 import com.sw300.community.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 
 
 @Controller
@@ -16,6 +19,8 @@ import java.security.Principal;
 public class MemberController {
 
     private final MemberService memberService;
+
+    private final CategoryService categoryService;
 
     @GetMapping("/")
     public String mainLogin(){
@@ -28,8 +33,11 @@ public class MemberController {
     }
 
     @GetMapping("/home")
-    public String home(){
-        return "home";
+    public void home(Model model){
+
+        List<CategoryDto> dtoList = categoryService.getBestCategory();
+
+        model.addAttribute("dtoList", dtoList);
     }
 
     @GetMapping("/myPage")
