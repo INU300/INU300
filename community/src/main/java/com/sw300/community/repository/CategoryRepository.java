103b204d6c70f481c34800c9c275493801fe60c7
hen715 <hen715@naver.com>
2023-10-15 03:25:56 +0900

    카테코리 이름으로 찾기 추가

--- community/src/main/java/com/sw300/community/repository/CategoryRepository.java
+++ community/src/main/java/com/sw300/community/repository/CategoryRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     @Query("SELECT c FROM Category c ORDER BY c.dailyVisitors DESC")
     List<Category> findByDailyVisitors(Pageable pageable);
+    Optional<Category> findByName(String name);
 }
