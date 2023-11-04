a75d278fbdd2b508f1460bef59e00eb3585f54d8
hann0079 <haeun8701@gmail.com>
2023-11-04 23:42:23 +0900

    refactor

--- community/src/main/java/com/sw300/community/board/service/ExternalService.java
+++ community/src/main/java/com/sw300/community/board/service/ExternalService.java
@@ -1,20 +1,5 @@
 package com.sw300.community.board.service;
 
-import com.sw300.community.board.common.ServiceResult;
-import com.sw300.community.board.dto.BoardDTO;
-import com.sw300.community.board.dto.BoardInput;
-import com.sw300.community.board.enums.LikeStatus;
-import com.sw300.community.board.model.Board;
-import com.sw300.community.dto.PageRequestDto;
-import com.sw300.community.dto.PageResponseDto;
-import com.sw300.community.member.dto.MemberCategoryDto;
-import java.util.List;
-import java.util.Map;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-
 public interface ExternalService {
 
     /**
