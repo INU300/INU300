b774ea7d0de66a54fb72ba95846aeb7bd8c8ecdb
hann0079 <haeun8701@gmail.com>
2023-11-06 20:58:28 +0900

    feat: 댓글 조회 및 작성 구현

---
+++ community/src/main/resources/static/js/reply.js
@@ -0,0 +1,37 @@
+
+async function getList({bno, page, size, goLast}){
+
+    const result = await axios.get(`/api/reply/list/${bno}`, {params: {page, size}})
+
+    if(goLast){
+        const total = result.data.total
+        const lastPage = parseInt(Math.ceil(total/size))
+
+        return getList({bno:bno, page:lastPage, size:size})
+
+    }
+
+    return result.data
+}
+
+
+async function addReply(replyObj) {
+    const response = await axios.post(`/api/reply`,replyObj)
+    return response.data
+}
+
+async function getReply(rno) {
+    const response = await axios.get(`/api/reply/${rno}`)
+    return response.data
+}
+
+async function modifyReply(replyObj) {
+
+    const response = await axios.put(`/api/reply/type/${replyObj.rno}`, replyObj)
+    return response.data
+}
+
+async function removeReply(rno) {
+    const response = await axios.post(`/api/reply/type/${rno}`)
+    return response.data
+}
